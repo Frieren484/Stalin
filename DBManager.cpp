f6ffe2eac70bcfd729a0f0ad5891469e5c32ad84
Your Name <you@example.com>
2026-03-25 11:22:58 +0400

    ефакторинг: выделение DBManager в отдельные файлы и локализация ошибок

--- DBManager.cpp
+++ DBManager.cpp
@@ -1,14 +1,14 @@
-#include "DBManager.h"
+﻿#include "DBManager.h"
 #include <iostream>
 
 using namespace std;
 
 DBManager::DBManager() : hEnv(SQL_NULL_HENV), hDbc(SQL_NULL_HDBC) {
     if (SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &hEnv) != SQL_SUCCESS) {
-        cerr << "[Ошибка] Не удалось выделить дискриптор окружения." << endl;
+        cerr << "[Ш] шибка выделения дескриптора окружения (ENV)." << endl;
     }
     if (SQLSetEnvAttr(hEnv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0) != SQL_SUCCESS) {
-        cerr << "[Ошибка] Не удалось установить версию ODBC." << endl;
+        cerr << "[Ш] шибка установки версии ODBC." << endl;
     }
 }
 
@@ -21,27 +21,22 @@ DBManager::~DBManager() {
 
 bool DBManager::Connect(const string& databaseName) {
     if (SQLAllocHandle(SQL_HANDLE_DBC, hEnv, &hDbc) != SQL_SUCCESS) {
-        cerr << "[Ошибка] Не удалось выделить дискриптор подключения." << endl;
+        cerr << "[Ш] шибка выделения дескриптора DBC." << endl;
         return false;
     }
 
-    // Формируем строку подключения для SQLEXPRESS
     string connStr = "DRIVER={SQL Server};SERVER=DESKTOP-SR5B112\\SQLEXPRESS;DATABASE=" + databaseName + ";Trusted_Connection=yes;";
-    
+    wstring wConnStr(connStr.begin(), connStr.end());
     SQLWCHAR outConnStr[1024];
     SQLSMALLINT outConnStrLen;
-
-    // Конвертация string в wstring для SQLDriverConnectW
-    wstring wConnStr(connStr.begin(), connStr.end());
 
     SQLRETURN ret = SQLDriverConnectW(hDbc, NULL, (SQLWCHAR*)wConnStr.c_str(), SQL_NTS, outConnStr, 1024, &outConnStrLen, SQL_DRIVER_NOPROMPT);
 
     if (SQL_SUCCEEDED(ret)) {
-        cout << "[DB] Успешное подключение к базе: " << databaseName << endl;
+        cout << "[DB] одключено к " << databaseName << endl;
         return true;
-    }
-    else {
-        cout << "[DB] Ошибка подключения к базе " << databaseName << endl;
+    } else {
+        cerr << "[DB] шибка подключения к " << databaseName << endl;
         PrintError(SQL_HANDLE_DBC, hDbc);
         return false;
     }
@@ -61,22 +56,21 @@ bool DBManager::ChangeDatabase(const string& newDatabaseName) {
 }
 
 bool DBManager::Ping() {
-    if (hDbc == SQL_NULL_HDBC) return false;
+    if (hDbc == SQL_NULL_HDBC) {
+        cout << "[PING] Соединение отсутствует." << endl;
+        return false;
+    }
 
-    SQLWCHAR state[6];
-    SQLWCHAR msg[SQL_MAX_MESSAGE_LENGTH];
+    SQLWCHAR state[6], msg[SQL_MAX_MESSAGE_LENGTH];
     SQLINTEGER native;
     SQLSMALLINT msgLen;
-
-    // Пытаемся получить диагностическую запись, чтобы проверить живо ли соединение
     SQLRETURN ret = SQLGetDiagRecW(SQL_HANDLE_DBC, hDbc, 1, state, &native, msg, SQL_MAX_MESSAGE_LENGTH, &msgLen);
 
     if (ret == SQL_NO_DATA) {
         cout << "[PING] Соединение активно." << endl;
         return true;
-    }
-    else {
-        wcout << L"[PING] Проблема с соединением. Состояние: " << state << endl;
+    } else {
+        wcout << L"[PING] шибка! Состояние: " << state << L" : " << msg << endl;
         return false;
     }
 }
@@ -86,8 +80,7 @@ void DBManager::PrintError(SQLSMALLINT handleType, SQLHANDLE handle) {
     SQLINTEGER native;
     SQLSMALLINT msgLen;
     int i = 1;
-
     while (SQLGetDiagRecW(handleType, handle, i++, state, &native, msg, SQL_MAX_MESSAGE_LENGTH, &msgLen) != SQL_NO_DATA) {
-        wcout << L"SQL Error: " << state << L" : " << msg << endl;
+        wcout << L"SQL-ошибка: " << state << L" : " << msg << endl;
     }
 }
--- DBManager.h
+++ DBManager.h
@@ -1,1 +1,22 @@
-#pragma once
+﻿#pragma once
+#include <windows.h>
+#include <sql.h>
+#include <sqlext.h>
+#include <string>
+
+class DBManager {
+private:
+    SQLHENV hEnv;
+    SQLHDBC hDbc;
+    void PrintError(SQLSMALLINT handleType, SQLHANDLE handle);
+
+public:
+    DBManager();
+    ~DBManager();
+
+    bool Connect(const std::string& dbName);
+    void Disconnect();
+    bool Ping();
+    bool ChangeDatabase(const std::string& newDb);
+    SQLHDBC GetConnection() { return hDbc; }
+};
